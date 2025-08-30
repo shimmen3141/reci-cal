882ad199602868a030832635bc3b0a27b6182493
shimmen3141 <y.shin3141@gmail.com>
2025-08-30 18:44:25 +0900

    fix:PageLayoutを修正 #30

--- src/components/layout/PageLayout.tsx
+++ src/components/layout/PageLayout.tsx
@@ -1,12 +1,13 @@
 import { PageHeader } from "./PageHeader/PageHeader";
 import { PageFooter } from "./PageFooter/PageFooter";
 import { PageContentsArea } from "./PageContentsArea/PageContentsArea";
+import styles from './PageLayout.module.scss';
 
 type Props = { children: React.ReactNode };
 
 export const PageLayout = ({ children }: Props) => {
   return (
-    <div className="pageLayout">
+    <div className={styles.pageLayout}>
       <PageHeader />
       <PageContentsArea>{children}</PageContentsArea>
       <PageFooter />
