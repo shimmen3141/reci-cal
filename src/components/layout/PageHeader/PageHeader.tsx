7735bfdb7188314cf892535afc54865ef4c37085
shimmen3141 <y.shin3141@gmail.com>
2025-08-30 18:18:28 +0900

    add:PageHeaderにscssを追加 #30

--- src/components/layout/PageHeader/PageHeader.tsx
+++ src/components/layout/PageHeader/PageHeader.tsx
@@ -1,5 +1,11 @@
+import styles from './PageHeader.module.scss';
+
 type PageHeaderProps = { title?: string };
 
 export const PageHeader = ({ title = "ReciCal" }: PageHeaderProps) => {
-  return <div>{title}</div>;
+  return (
+    <div className={styles.appHeader}>
+      <div className={styles.title}>{title}</div>
+    </div>
+  );
 };
